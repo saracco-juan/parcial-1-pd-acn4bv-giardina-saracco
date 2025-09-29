77469123dcc126be448cdabf73b529b365fae784
Mariano <marianogiardinamonti1@gmail.com>
2025-09-29 19:06:51 -0300

    Creado objeto Categoria

---
+++ categorias.js
@@ -0,0 +1,27 @@
+class Categoias {
+    
+    constructor(nombre, colorCard, colorTexto) {
+
+        this.nombre = nombre;
+
+        this.colorCard = colorCard;
+
+        this.colorTexto = colorTexto;
+
+    }
+
+    getNombre() {
+        return this.nombre;
+    }
+
+    getColorCard() {
+        return this.colorCard;
+    }
+
+    getColorTexto() {
+        return this.colorTexto;
+    }
+
+
+}
+
