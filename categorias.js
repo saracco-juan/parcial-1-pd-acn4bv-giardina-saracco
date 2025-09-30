3f1900b72c1e6585c5b10393e719c596ff96c852
juan-saracco <juansaracco1212@gmail.com>
2025-09-29 21:36:20 -0300

    Feature: Fonts added to categories. Structure enhaced.

--- categorias.js
+++ categorias.js
@@ -1,12 +1,14 @@
 class Categoria {
-    
-    constructor(nombre, colorCard, colorTexto) {
+
+    constructor(nombre, colorCard, colorTexto, fontFamily) {
 
         this.nombre = nombre;
 
         this.colorCard = colorCard;
 
         this.colorTexto = colorTexto;
+
+        this.fontFamily = fontFamily;
 
     }
 
@@ -25,11 +27,11 @@ class Categoria {
     generarCard() {
         return `
         <div class="${this.colorCard} rounded-2xl p-8 ${this.colorTexto} hover:scale-105 transition-transform cursor-pointer">
-            <div class="text-6xl font-bold mb-4">Aa</div>
-            <h3 class="text-xl font-semibold">${this.nombre}</h3>
+            <div class="text-6xl font-bold mb-4 ${this.fontFamily}">Aa</div>
+            <h3 class="text-xl font-semibold ${this.fontFamily}">${this.nombre}</h3>
         </div>
         `;
-    }    
+    }
 }
 
 const COLOR_CARD_AMARILLA = "bg-amarillo";
@@ -37,8 +39,8 @@ const COLOR_CARD_AMARILLA = "bg-amarillo";
 const COLOR_CARD_OSCURA = "bg-card-bg";
 
 let categorias = [
-    new Categoria("MODERNA", COLOR_CARD_AMARILLA, "text-black"),
-    new Categoria("ELEGANTE", COLOR_CARD_AMARILLA, "text-black"),
-    new Categoria("CLÁSICA", COLOR_CARD_OSCURA, "border border-gray-700"),
-    new Categoria("CREATIVA", COLOR_CARD_OSCURA, "border border-gray-700"),
+    new Categoria("MODERNA", COLOR_CARD_AMARILLA, "text-black", "sansation"),
+    new Categoria("ELEGANTE", COLOR_CARD_AMARILLA, "text-black", "playfair-display"),
+    new Categoria("CLÁSICA", COLOR_CARD_OSCURA, "border border-gray-700", "roboto"),
+    new Categoria("CREATIVA", COLOR_CARD_OSCURA, "border border-gray-700", "montserrat"),
 ];
